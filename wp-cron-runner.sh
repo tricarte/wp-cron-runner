eaf79b41458e36a077e95b8653292db6f43765b2
tricarte <sonagirbas@gmail.com>
2022-03-27 18:54:50 +0300

    fix formatting

--- wp-cron-runner.sh
+++ wp-cron-runner.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 if [[ -d /etc/nginx/conf.d ]]; then
-	mapfile -t VHOSTS < <(grep -iRls server_name /etc/nginx/conf.d)
+    mapfile -t VHOSTS < <(grep -iRls server_name /etc/nginx/conf.d)
 fi
 
 for host in "${VHOSTS[@]}"
 do
-	# Parse root directive in vhost config file
-	root_line=$( grep -s -E '^[[:space:]]*?root[[:space:]]{1}[a-zA-Z0-9\/\.\-]+;$' "$host" )
+    # Parse root directive in vhost config file
+    root_line=$( grep -s -E '^[[:space:]]*?root[[:space:]]{1}[a-zA-Z0-9\/\.\-]+;$' "$host" )
     root_line=$(echo $root_line)
     root=$(echo "$root_line" | cut -d" " -f2 | tr -d ';')
 
